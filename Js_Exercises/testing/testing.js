11362711b96dbfb2dcb870a4b69f65677dd768d6
DucTran2511 <emlatruongtansang@gmail.com>
2025-06-19 15:08:03 +0700

    to do list

---
+++ Js_Exercises/testing/testing.js
@@ -0,0 +1,5 @@
+const elementsByClass = document.querySelectorAll('.highlighted');
+const elementsById = document.getElementById("hehe")
+console.log(elementsByClass);
+alert(elementsByClass[0]);
+alert(elementsById);
